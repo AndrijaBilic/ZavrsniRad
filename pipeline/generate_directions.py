fbe8f4658615343918aa73385f26ea4abbaef59a
Andrija <andrija.bilic.ab@gmail.com>
2026-05-12 16:27:39 +0200

    add torch no grad, still out of memmory

--- pipeline/generate_directions.py
+++ pipeline/generate_directions.py
@@ -35,11 +35,13 @@ def get_mean_activations(model, tokenizer, instructions, tokenize_instructions_f
     for i in tqdm(range(0, len(instructions), batch_size)):
         inputs = tokenize_instructions_fn(instructions=instructions[i:i+batch_size])
 
-        with add_hooks(module_forward_pre_hooks=fwd_pre_hooks, module_forward_hooks=[]):
-            model(
-                input_ids=inputs.input_ids.to(model.device),
-                attention_mask=inputs.attention_mask.to(model.device),
-            )
+        # ADD torch.no_grad() HERE
+        with torch.no_grad(): 
+            with add_hooks(module_forward_pre_hooks=fwd_pre_hooks, module_forward_hooks=[]):
+                model(
+                    input_ids=inputs.input_ids.to(model.device),
+                    attention_mask=inputs.attention_mask.to(model.device),
+                )
 
     return mean_activations
 
